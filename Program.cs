229b7ed44ad675c56759e2ca4c96303b35251914
Shan Konduru <ShanKonduru@users.noreply.github.com>
2022-02-06 17:46:08 +0530

    Codebase

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShanCoin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
