5eb3cf481b97fd63140b136ee523075f95090591
Shan Konduru <ShanKonduru@users.noreply.github.com>
2022-02-06 20:24:34 +0530

    commit

--- Program.cs
+++ Program.cs
@@ -1,8 +1,22 @@
-﻿using System;
+﻿using System.Linq;
+using System.Collections.Generic;
+using System;
 namespace ShanCoin {
     public class Program {
         static void Main (string[] args) {
-            Console.WriteLine ("Hello World!");
+            Random rnd = new Random(DateTime.Now.Millisecond);
+            IBlock genesis =  new Block(new byte[]{0x00, 0x00, 0x00, 0x00, 0x00 });
+            byte[] difficulty = new byte[]{0x00, 0x00};
+
+            Blockchain chain = new Blockchain(difficulty, genesis);
+            for(int i=0; i<200; i++){
+                var data = Enumerable.Range(0, 2256).Select(p=> (byte) rnd.Next());
+                chain.Add(new Block(data.ToArray()));
+                Console.WriteLine(chain.LastOrDefault().ToString());
+                Console.WriteLine($"Chain is Valid :{chain.IsValid()}");
+            }
+
+            Console.ReadLine();
         }
     }
 }
