da00871e73cf310c4a035d6c46e9b2d82b583827
Shan Konduru <ShanKonduru@users.noreply.github.com>
2022-02-06 19:38:15 +0530

    Block chain class added

---
+++ Blockchain.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShanCoin {
+    public class Blockchain : IEnumerable<IBlock> {
+        private List<IBlock> _items = new List<IBlock> ();
+        public Blockchain (byte[] difficulty, IBlock genesis) {
+            Difficulty = difficulty;
+            genesis.Hash = genesis.MineBlock (difficulty);
+            Items.Add (genesis);
+        }
+
+        public void Add (IBlock block) {
+            if (Items.LastOrDefault () != null) {
+                block.PrevHash = Items.LastOrDefault ().Hash;
+            }
+            block.Hash = block.MineHash (Difficulty);
+            Items.Add (block);
+        }
+
+        public int Count => Items.Count;
+
+        public IBlock this [index] {
+            get => Items[index];
+            set => Items[index] = value;
+        }
+
+        public byte[] Difficulty { get; }
+        public List<IBlock> Items {
+            get => _items;
+            set => _items = value;
+        }
+
+        IEnumerator<IBlock> IEnumerable<IBlock>.GetEnumerator () {
+            return Items.GetEnumerator ();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator () {
+            return Items.GetEnumerator ();
+        }
+    }
+}
--- IBlock.cs
+++ IBlock.cs
@@ -2,12 +2,10 @@ using System;
 
 namespace ShanCoin {
     public interface IBlock {
-        /*
         public byte[] Data { get; }
         public byte[] Hash { get; set; }
         public long Nonce { get; set; }
         public byte[] PrevHash { get; set; }
         public DateTime TimeStamp { get; }
-        */
     }
 }
